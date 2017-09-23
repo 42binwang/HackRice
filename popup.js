ca7b234623e028b913a8307988b451e29ea551cf
张云翔 <994498752@qq.com>
2017-09-23 05:43:07 -0500

    Add simple html for setting, not completed

--- popup.js
+++ popup.js
@@ -1,56 +1,12 @@
-chrome.runtime.onMessage.addListener(function (request, sender) {
-    if (request.action == "getSource") {
-        chrome.storage.local.set({ "sourceMsg": request.source }, function () {
-            message.innerText = request.source;
-            filterMsg();
-        });
-    }
+var setting;
 
-    if (request.action == "filterWord") {
-        chrome.storage.local.set({ "keyWord": request.source }, function () {
-            logKeyWord();
-        })
-    }
-});
-
-function filterMsg() {
-    chrome.tabs.executeScript(null, {
-        file: "FilterKeyWord.js"
-    },
-    function () {
-        if (chrome.runtime.lastError) {
-            message.innerText = 'There was an error filterMsg script : \n' + chrome.runtime.lastError.message;
-        }
-    });
+window.onload = function () {
+    setting = document.querySelector('#setting');
+    console.warn(setting.click)
+    setting.onclick = function () { test(); };
 }
 
-function logKeyWord() {
-    chrome.tabs.executeScript(null, {
-        file: "logKeyWords.js"
-    }),
-    function () {
-        if (chrome.runtime.lastError) {
-            message.innerText = 'There was an error filterMsg script : \n' + chrome.runtime.lastError.message;
-        }
-    }
+function test() {
+    var newURL = "setting.html";
+    chrome.tabs.create({ url: newURL });
 }
-
-function getSource() {
-    chrome.tabs.executeScript(null, {
-        file: "getPagesSource.js"
-    },
-    function () {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-        if (chrome.runtime.lastError) {
-            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-        }
-    });
-}
-
-function onWindowLoad() {
-
-    var message = document.querySelector('#message');
-    getSource();
-}
-
-window.onload = onWindowLoad;
